8c547f0a036cdbcdc585fe7f142a23d29981f326
ГУЛЬФИЯ <Gulepha@mail.ru>
2025-03-12 20:17:28 +0500

    Add files via upload

---
+++ дз1.py
@@ -0,0 +1,11 @@
+n = int(input('номер месяца?:'))
+if n==1 or n == 2 or n==12:
+    print ('зима')
+elif n== 3 or n == 4 or n == 5:
+    print('весна')
+elif n==6 or n == 7 or n==8:
+    print ('лето')
+elif n==9 or n == 10 or n==11:
+    print ('осень')
+else:
+    print()
